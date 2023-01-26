b9d6fb0ebdee512b5a1fa6f514d93c57e519930b
cojomaru <100784118+cojamaru@users.noreply.github.com>
2023-01-26 23:08:17 +0100

    [Rework] Setup of project
    Setup of projects structure
    User Controller can login user (with cookies for now, but can be changed)
    User Controller can register user
    User Controller can start resetting the password
    User Controller can end resetting the password
    User, Projects and Claims seeded
    User elevation of authorization possible through the claims
    Initial migration setup
    
    TODO:
    There is an EFCore Claim model available from Identity check it out
    Password resetting token might need an expiration time
    Define process of elevating the user
    Start thinking about tests

---
+++ AggAPIs/UserHelper.cs
@@ -0,0 +1,25 @@
+﻿using Datalayer.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AggAPIs
+{
+    public class UserHelper
+    {
+        public static ClaimsPrincipal Convert(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("userid", user.UserId.ToString()),
+                new Claim("useremail", user.Email)
+            };
+
+            claims.AddRange(user.Claims.Select(x => new Claim(x.Type, x.Value)));
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            return new ClaimsPrincipal(identity);
+        }
+    }
+}
